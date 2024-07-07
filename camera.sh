4876b27ea408b481449acaf2719077556d172217
Christopher-Mee <70412094+Christopher-Mee@users.noreply.github.com>
2024-07-07 00:22:17 -0400

    Update camera.sh
    Remove system specific modifications.

--- camera.sh
+++ camera.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 DATE=$(date +"%Y-%m-%d_%H%M")
-raspistill -hf -vf -o /home/pi/timelapse/$DATE.jpg
-#raspistill -o /home/pi/timelapse/$DATE.jpg
+raspistill -o /home/pi/timelapse/$DATE.jpg
